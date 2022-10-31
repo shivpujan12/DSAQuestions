fce2ae375a9cd987c5f3da5add25d042c4ef5347
Shivpujan yadav <31546372+shivpujan12@users.noreply.github.com>
2022-10-31 14:18:41 +0530

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -8,25 +8,30 @@ class GFG {
 		//code
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for(int i=0;i<n;i++){
-		    //create the array
-		    arr[i] = sc.nextInt();
-		}
-		
-		//Reversing the array
-		n = n-1;
-        for(int i=0;i<=n/2 ;i++){
-            int temp = arr[i];
-            arr[i] = arr[n-i];
-            arr[n-i] = temp;
-       }
-       
-       //printing the array
-       for(int i=0;i<arr.length;i++){
-           System.out.print(arr[i] + " ");
-       }
+	
+		for(int j=0;j<t;j++){
+		    int n = sc.nextInt();
+		    int arr[] = new int[n];
+		    
+    		for(int i=0;i<n;i++){
+    		    //create the array
+    		    arr[i] = sc.nextInt();
+    		}
+    		
+    		//Reversing the array
+    		n = n-1;
+            for(int i=0;i<=n/2 ;i++){
+                int temp = arr[i];
+                arr[i] = arr[n-i];
+                arr[n-i] = temp;
+           }
+           
+           //printing the array
+           for(int i=0;i<arr.length;i++){
+               System.out.print(arr[i] + " ");
+           }
+           System.out.println();
+	    }
 
 	}
 }
