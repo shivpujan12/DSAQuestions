6d5ac5e46fc3b0e176e7ddb50d821fb462bd29e9
Shivpujan yadav <31546372+shivpujan12@users.noreply.github.com>
2022-10-31 14:05:06 +0530

    Create ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++){
+		    //create the array
+		    arr[i] = sc.nextInt();
+		}
+		
+		//Reversing the array
+		n = n-1;
+        for(int i=0;i<=n/2 ;i++){
+            int temp = arr[i];
+            arr[i] = arr[n-i];
+            arr[n-i] = temp;
+       }
+       
+       //printing the array
+       for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i] + " ");
+       }
+
+	}
+}
